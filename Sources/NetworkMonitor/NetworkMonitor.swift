55c05dcae6617b24b3fad8a43b5261600c9dcd70
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-12-18 06:31:30 +0530

    first commit

---
+++ Sources/NetworkMonitor/NetworkMonitor.swift
@@ -0,0 +1,47 @@
+
+import Foundation
+import Network
+
+public class NetworkMonitor: ObservableObject {
+  
+  static public let shared = NetworkMonitor()
+  
+  private let monitor = NWPathMonitor()
+  private let queue = DispatchQueue(label: "network_Monitor")
+  
+  public var isActive = false
+  public var isExpensive = false
+  public var isConstrained = false
+  public var connectionType = NWInterface.InterfaceType.other
+  
+  private init() {
+    monitor.pathUpdateHandler = { [weak self] path in
+      self?.updateStatus(using: path)
+    }
+    monitor.start(queue: queue)
+  }
+  
+  private func updateStatus(using path: NWPath) {
+    isActive = path.status == .satisfied
+    isExpensive = path.isExpensive
+    isConstrained = path.isConstrained
+    
+    let connectionTypes = [NWInterface.InterfaceType.cellular, .wifi, .wiredEthernet]
+    self.connectionType = connectionTypes.first(where: path.usesInterfaceType) ?? .other
+    
+    DispatchQueue.main.async {
+      self.objectWillChange.send()
+    }
+  }
+}
+
+extension NetworkMonitor: CustomStringConvertible, CustomDebugStringConvertible {
+  
+  public var description: String {
+    "Network: \(connectionType),  Status: \(isActive ? "Connected" : "Disconnected")"
+  }
+  
+  public var debugDescription: String {
+    "Network: \(connectionType), Status: \(monitor.currentPath.status)"
+  }
+}
